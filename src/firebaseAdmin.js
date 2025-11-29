758f74b7a36326b00e5a82e984cb36712fa27a32
hammPa <m.ilhamcoc70@gmail.com>
2025-11-30 00:42:27 +0700

    edit firebaseadmin

--- src/firebaseAdmin.js
+++ src/firebaseAdmin.js
@@ -1,10 +1,16 @@
 const admin = require('firebase-admin');
-const path = require('path');
+
+const serviceAccountString = process.env.FIREBASE_ADMIN_CREDENTIAL;
+
+// Cek apakah variable terbaca (Debugging)
+if (!serviceAccountString) {
+  throw new Error("Env variable FIREBASE_ADMIN_CREDENTIAL tidak ditemukan atau kosong!");
+}
+
+const serviceAccount = JSON.parse(serviceAccountString);
 
 admin.initializeApp({
-    credential: admin.credential.cert(
-        require(path.join(__dirname, "..", process.env.FIREBASE_ADMIN_CREDENTIAL))
-    ),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
